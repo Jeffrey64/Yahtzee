eed8ed2b15b5f0e5c1e0e3d3bf6cf091a5246dcf
Jeffrey Lagunas <jeffrey.lagunas@mrjaffesclass.com>
2023-12-07 11:39:15 -0800

    summarize

--- Dice.java
+++ Dice.java
@@ -1,13 +1,11 @@
 
 public class Dice
 {
-    
     Die6 Die1; 
     Die6 Die2;
     Die6 Die3;
     Die6 Die4;
     Die6 Die5;
-    
     public Dice()
     {
         Die1 = new Die6();
@@ -20,14 +18,17 @@ public class Dice
     public void roll() { 
         Die1.roll();
         Die2.roll();
+        Die3.roll();
+        Die4.roll();
+        Die5.roll();
     }
 
-    public int getvalue() {
+    public int getValue() {
         return Die1.getValue()+Die2.getValue();
     }
 
     public int rollAndGetValue() {
         roll();
-        return getvalue();
+        return getValue();
     }
 }
